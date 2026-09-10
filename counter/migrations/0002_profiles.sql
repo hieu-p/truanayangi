647a8859ad851f0b733a871c341f20d7eb6a2923
unknown <hieupah123@gmail.com>
2026-09-10 13:52:15 +0700

    Replace old website with new version

--- counter/migrations/0002_profiles.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS user_food_profiles (
- user_sub TEXT PRIMARY KEY,
- profile TEXT NOT NULL,
- revision INTEGER NOT NULL DEFAULT 1,
- created_at INTEGER NOT NULL DEFAULT (unixepoch()),
- updated_at INTEGER NOT NULL DEFAULT (unixepoch())
-);
